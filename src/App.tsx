dfebb5066ce1b540eed4e4adb366208abb35ef3a
KingAJ19 <anelesokwane@gmail.com>
2023-12-01 09:21:27 +0200

    fixed button

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ const App = () => {
       <form className="note-form">
         <input placeholder="title" required/> 
         <textarea placeholder="Content" rows={10} required></textarea>
-        <button type="submit"></button>
+        <button type="submit">
+          Add Note
+        </button>
       </form>
       <div className="notes-grid">
         <div className="note-item">
-          <div className="notes-header"> 
+          <div className="notes-header">
              <button>x</button>
           </div>
           <h2>Note Title</h2>
